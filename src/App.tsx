e4a7d1f97c9d3afb331bbfcb7910687c86a38153
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-12-16 22:05:32 +0530

    * Refactor Pulsar component * Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import { CircleStop, Pause, Play, Volume2 } from 'lucide-react';
+import { CircleStop, Pause, Play, Volume2, VolumeX } from 'lucide-react';
 import './App.css';
 import Button from './components/Button';
 import Pulsar from './components/Pulsar';
+import useBreathingEngine from './hooks/useBreathingEngine';
+import { useState } from 'react';
 
 function App() {
+    const { currentPhase, toggle, isActive, reset, progress, timeLeft, duration } = useBreathingEngine();
+    const [isMute, setIsMute] = useState(false);
+
+    function volume() {
+        setIsMute((prev) => !prev);
+    }
+
     return (
-        <div className="flex items-center flex-col gap-20">
+        <div className="flex flex-col items-center gap-30">
             <h1 className="text-3xl font-bold">Luminous Pulse</h1>
-            <Pulsar />
-            <div className="flex justify-center gap-30">
-                <Button label={<Play />} onClick={() => {}} />
-                <Button label={<Pause />} onClick={() => {}} />
-                <Button label={<CircleStop />} onClick={() => {}} />
-                <Button label={<Volume2 />} onClick={() => {}} />
+            <Pulsar phase={currentPhase} progress={progress} timeLeft={timeLeft} duration={duration} />
+            <div className="flex justify-center gap-20">
+                <Button
+                    className={isActive ? 'bg-yellow-300' : 'bg-green-300'}
+                    label={isActive ? <Pause /> : <Play />}
+                    onClick={() => toggle()}
+                />
+                <Button className="bg-red-300" label={<CircleStop />} onClick={() => reset()} />
+                <Button className="bg-orange-300" label={isMute ? <VolumeX /> : <Volume2 />} onClick={() => volume()} />
             </div>
         </div>
     );
--- src/components/Pulsar.tsx
+++ src/components/Pulsar.tsx
@@ -1,10 +1,33 @@
-interface pulsarProps {}
+import { motion, type Variants } from 'framer-motion';
+import type { BreathPhase } from '../types';
 
-const Pulsar = ({}: pulsarProps) => {
+interface pulsarProps {
+    phase: BreathPhase;
+    progress?: number;
+    timeLeft: number;
+    duration: number;
+}
+
+const variantsMap: Variants = {
+    Inhale: { scale: 1.5 },
+    'Hold Full': { scale: 1.5 },
+    Exhale: { scale: 1.0 },
+    'Hold Empty': { scale: 1.0 },
+};
+
+const Pulsar = ({ phase = 'Inhale', timeLeft = 0, duration }: pulsarProps) => {
     return (
-        <div className="h-60 w-60 rounded-full border-white border flex justify-center items-center">
-            <div className="rounded-full pulsar"></div>
-        </div>
+        <motion.div
+            className="h-60 w-60 rounded-full border-white border flex justify-center items-center"
+            variants={variantsMap}
+            animate={phase}
+            transition={{ duration: duration / 1000, ease: 'easeInOut' }}
+        >
+            <div className="rounded-full pulsar w-40 h-40 bg-linear-to-br from-neon-cyan to-neon-mint shadow-[0_0_60px_-10px_rgba(34,211,238,0.6)] flex flex-col justify-center items-center">
+                <p className="text-white text-3xl font-bold">{phase}</p>
+                <p className="text-white text-2xl font-bold">{Math.ceil(timeLeft / 1000)} s</p>
+            </div>
+        </motion.div>
     );
 };
 
