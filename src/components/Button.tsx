83f6b0aec0d92120707805200a82fd7ed6d7e71d
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-12-16 22:03:59 +0530

    * Implement Button Component * Remove Unnecessary styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button = ({ className, label = '', onClick }: ButtonProps) => {
     return (
-        <button className={`btn ${className}`} onClick={onClick}>
+        <button className={`btn cursor-pointer rounded-3xl px-8 py-2 text-black ${className}`} onClick={onClick}>
             {label}
         </button>
     );
