27c1a38a8e8407c0b267f5a46f9849fee1acb90e
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-12-24 14:18:02 +0530

    * Add a value prop to the BreathingOptions component * Add a duration prop to the PresetSlider component

--- src/components/BreathingOptions.tsx
+++ src/components/BreathingOptions.tsx
@@ -1,18 +1,19 @@
 interface BreathingOptionsProps {
+    value: string;
     selected: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     label: string;
 }
 
-const BreathingOptions = ({ selected, onChange, label }: BreathingOptionsProps) => {
+const BreathingOptions = ({ value, selected, onChange, label }: BreathingOptionsProps) => {
     return (
         <div className="flex items-center gap-2">
             <input
                 type="radio"
                 name="breathing-options"
                 id="box-radio"
-                value="box"
-                checked={selected === 'box'}
+                value={value}
+                checked={selected === value}
                 onChange={onChange}
             />
             <label htmlFor="box-radio">{label}</label>
--- src/components/PresetSlider.tsx
+++ src/components/PresetSlider.tsx
@@ -1,13 +1,15 @@
 interface PresetSliderProps {
     label: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    duration: number;
 }
 
-const PresetSlider = ({ label, onChange }: PresetSliderProps) => {
+const PresetSlider = ({ label, onChange, duration }: PresetSliderProps) => {
     return (
         <div className="flex justify-between px-5">
             <label htmlFor="inhale-slider">{label}</label>
             <input type="range" name="slider" id="inhale-slider" min="0" max="15" value={0} onChange={onChange} />
+            <span>{duration}s</span>
         </div>
     );
 };
